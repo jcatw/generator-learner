56819f8d94cd07157c1067d8cdabcad2a1f3ee83
James Atwood <jatwood@cs.umass.edu>
2013-01-29 09:51:52 -0500

    running but not converging

--- gglearn.py
+++ gglearn.py
@@ -1,9 +1,68 @@
 import numpy as np
 import networkx as nx
 import scikits.statsmodels.api as sm
+import matplotlib.pyplot as plt
+import ols
+from copy import deepcopy
 
-def Q_fn_gen(basis, weights):
-    return lambda x: basis.expand(x) * weights
+#def Q_fn_gen(basis, weights):
+#    return lambda x, basis=basis, weights=weights: (basis.expand(x) * weights).sum()
+
+class Q_fn:
+    def __init__(self, basis, weights):
+        self.basis = basis
+        self.weights = weights
+
+    def eval(self, x):
+        return (self.basis.array_expand(x) * self.weights).sum()
+
+class episode:
+    def __init__(self, learner, alpha, gamma, epsilon):
+        self.learner = learner
+        self.G = None
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+
+    def run(self, draw_steps = False):
+        print self.learner.actions.action_dict
+        Q = 0.0
+        action_taken = np.random.randint(0, len(self.learner.actions))
+        for i in xrange(self.learner.max_rows):
+
+            if not i%100: print i
+            #print i
+
+            self.learner.actions.get(action_taken).execute(self.learner.G)
+            if draw_steps:
+                print "Action taken: %s" % (self.learner.actions.action_dict[action_taken],)
+                nx.draw(self.learner.G)
+                plt.show()
+                #raw_input("Press Enter to continue")
+
+            feature_values = self.learner.features.get(self.learner.G)
+            self.learner.actions.get(action_taken).state.add_sample(feature_values, Q)
+
+            #print self.learner.actions.get(action_taken).state.design_matrix[self.learner.actions.get(action_taken).state.n-1], Q
+
+            reward = self.learner.reward_function(self.learner.G)
+
+            if self.learner.termination_function(reward): break
+
+            Q_values = self.learner.actions.Qs(feature_values)
+
+            if np.random.rand() <= self.epsilon:
+                action_taken = np.random.randint(0, len(self.learner.actions))
+            else:
+                action_taken = Q_values.argmax()
+
+            Q = (1 - self.alpha) * Q + self.alpha * (reward + self.gamma * Q_values[action_taken])
+
+        for i in xrange(len(self.learner.actions)):
+            self.learner.actions.get(i).compute_Q_fn()
+
+        self.G = self.learner.G
+
 
 class gglearner:
     def __init__(self,
@@ -13,54 +72,44 @@ class gglearner:
                  action_names,
                  basis_functions,
                  feature_functions,
-                 termination_tolerance,
+                 termination_function,
                  max_rows):
 
-        self.G = G0
-        self.reward_function
+        self.G0 = G0
+        self.G = deepcopy(G0)
+        #self.Gs = [G0]
+        self.episodes = []
+        self.reward_function = reward_function
         self.basis = basis(basis_functions)
         self.actions = actions(action_functions,
                                action_names,
-                               self.basis
-                               len(feature_functions)
+                               self.basis,
+                               len(feature_functions),
                                max_rows)
         self.features = features(feature_functions)
-        self.termination_tolerance
-        self.max_rows
+        self.termination_function = termination_function
+        self.max_rows = max_rows
 
-        
-
-    def episode(self, alpha, gamma, epsilon):
-        Q = 0.0
-        action_taken = np.random.randint(0, len(self.actions))
-        for i in xrange(self.max_rows):
-            feature_values = self.features.get(G)
-            self.actions.get(action_taken).state.add_sample(feature_values, Q)
-
-            reward = self.reward_function(G)
-
-            if reward <= self.termination_tolerance: break
-
-            Q_values = self.actions.rewards(feature_values)
-
-            if np.random.rand() <= epsilon:
-                action_taken = np.random.randint(0, len(self.actions))
-            else:
-                action_taken = Q_values.argmax()
-
-            Q = (1 - alpha) * Q + alpha * (reward + gamma * Q_values[action_taken])
-
-        for i in xrange(len(actions)):
-            actions.get(i).compute_Q_fn()
+    def run_episode(self ,alpha, gamma, epsilon, draw_steps=False):
+        new_episode = episode(self, alpha, gamma, epsilon)
+        new_episode.run(draw_steps)
+        self.episodes.append(new_episode)
+        self.G = deepcopy(self.G0)
 
 
 class features:
     def __init__(self, feature_functions):
+        # seriously weird shit: np.vectorize treats class instances as
+        # underlying dict, which breaks method calls.
+        # Just using list comprehensions.
+
+        #self.feature_functions = np.array(feature_functions)
+        #self.vfapply = np.vectorize(lambda f,x: f(x))
         self.feature_functions = feature_functions
-        self.vfapply = np.vectorize(lambda f,x: f(x))
 
     def get(self, G):
-        vfapply(feature_functions, G)
+        #self.vfapply(self.feature_functions, G)
+        return np.array([f(G) for f in self.feature_functions])
 
 class state:
     def __init__(self, max_rows, n_features, basis):
@@ -70,18 +119,27 @@ class state:
         self.Q = np.zeros(max_rows)
 
     def add_sample(self, feature_values, Q):
-        expanded_features = np.array(1)
-        for feature in feature_values:
-            expanded_features = append(expanded_features, basis.expand(feature))
-        design_matrix[self.n] = expanded_features
-        self.Q[n] = Q
+        expanded_features = np.zeros([len(feature_values),len(self.basis)])
+        for i,feature in enumerate(feature_values):
+            expanded_features[i] = self.basis.expand(feature)
+
+        self.design_matrix[self.n] = expanded_features.flatten()
+
+        self.Q[self.n] = Q
+
+        self.n += 1
+
 
     def regress(self):
-        self.design_matrix = self.design_matrix[:n]
-        self.Q = self.Q[:n]
+        self.design_matrix = self.design_matrix[:self.n]
+        self.Q = self.Q[:self.n]
 
-        res = sm.OLS(Q,design_matrix)
-        return res.weights
+        res = sm.OLS(self.Q,self.design_matrix)
+        fit = res.fit()
+        return fit.params
+        #self.weights = res.weights
+        #res = ols.ols(self.Q,self.design_matrix)
+        #return res.b
 
 class basis:
     def __init__(self, functions):
@@ -94,6 +152,12 @@ class basis:
     def expand(self, term):
         return self.vfapply(self.functions, term)
 
+    def array_expand(self, term_array):
+        expanded_array = np.zeros([len(term_array), len(self)])
+        for i,term in enumerate(term_array):
+            expanded_array[i] = self.expand(term)
+        return expanded_array.flatten()
+
 class action:
     def __init__(self, f, name, basis, num_features, max_size):
         self.f = f
@@ -102,14 +166,14 @@ class action:
         self.num_features = num_features
         self.max_size = max_size
         self.state = state(max_size, num_features, basis)
-        self.Q_function = Q_fn_gen(self.basis, np.zeros(num_features))
+        self.Q_function = Q_fn(self.basis, np.zeros(len(self.basis)*num_features))
 
     def execute(self, x):
         return self.f(x)
 
     def compute_Q_fn(self):
         weights = self.state.regress()
-        self.Q_function = Q_fn_gen(self.basis, weights)
+        self.Q_function = Q_fn(self.basis, weights)
         self.state = state(self.max_size, self.num_features, self.basis)
 
 class actions:
@@ -117,9 +181,9 @@ class actions:
         self.actions_list = [action(f, name, basis, num_features, max_size) for f, name in zip(function_iterable, names_iterable)]
         self.action_dict = {}
         for i,an_action in enumerate(self.actions_list):
-            self.action_dict[an_action.name] = i
+            self.action_dict[i] = an_action.name
 
-        self.vfapply = np.vectorize(lambda a,x: a.reward_function(x))
+        # self.vfapply = np.vectorize(lambda a,x: a.Q_function.eval(x))
 
     def __len__(self):
         return len(self.actions_list)
@@ -130,4 +194,5 @@ class actions:
         return self.actions_list[action_id]
 
     def Qs(self, feature_values):
-        return self.vfapply(self.action_list, feature_values)
+        #return self.vfapply(self.actions_list, feature_values)
+        return np.array([a.Q_function.eval(feature_values) for a in self.actions_list])
---
+++ plawindegree.py
@@ -0,0 +1,203 @@
+import gglearn as gg
+import numpy as np
+import numpy.random as rand
+from scipy import stats
+import networkx as nx
+import scikits.statsmodels.api as sm
+#import ols
+
+import matplotlib.pyplot as plt
+
+target_indegree_exponent = 2.7
+target_num_nodes = 1000
+
+initial_num_nodes = 3
+initial_num_edges = 2
+
+basis_functions = [lambda x: 1,
+                   lambda x: x,
+                   lambda x: x**2,
+                   lambda x: x**3,
+                   lambda x: x**4,
+                   lambda x: x**5]
+
+def reward_function_gen(target_exponent, target_num_nodes):
+    # no multiline lambdas => running regress once for
+    # each parameter.  Ewwwww.
+    return lambda G, target_exponent=target_exponent, target_num_nodes=target_num_nodes: fit_powerlaw_regress(G)[1] / (((np.abs(target_exponent - fit_powerlaw_regress(G)[0]) + 1) * np.log10(np.abs(G.number_of_nodes() - target_num_nodes) + 1))+1)
+
+
+
+
+def termination_function(reward):
+    return reward >= 0.9 and reward <= 1.1
+
+#------------------------------------------------------------------------------#
+# Utility Functions
+#------------------------------------------------------------------------------#
+def sample_pmf(pmf):
+    uniform_draw = rand.rand()
+    cmf = np.array(pmf).cumsum()
+    return np.array(range(len(cmf)))[uniform_draw < cmf][0]
+
+def get_in_degree(G):
+    return np.array(G.in_degree().values())
+
+def fit_powerlaw_MLE(G):
+    return stats.powerlaw.fit(get_in_degree(G))
+
+def fit_powerlaw_regress(G):
+    in_degree = get_in_degree(G)
+    n, bins = np.histogram(in_degree)
+
+    bins_midpoint = np.zeros(len(n))
+    for i in xrange(len(n)):
+        bins_midpoint[i] = (bins[i] + bins[i+1]) / 2.0
+
+    #reg_res = ols.ols(np.log(n+1), np.log(bins_midpoint))
+    reg_res = sm.OLS(np.log(n+1), np.array(sm.add_constant(np.log(bins_midpoint)))).fit()
+
+    #return reg_res.b[1],reg_res.R2
+    return reg_res.params[0], reg_res.rsquared
+
+#------------------------------------------------------------------------------#
+# Initialization Functions
+#------------------------------------------------------------------------------#
+def initial_graph(n_nodes, n_edges):
+    G = nx.DiGraph()
+    G.node_holes = [] #stack that tracks holes in sequential node numbering
+
+    for i in xrange(n_nodes):
+        G.add_node(G.number_of_nodes())
+
+    for i in xrange(n_edges):
+        edge_from = rand.randint(0, G.number_of_nodes())
+        edge_to = rand.randint(0, G.number_of_nodes())
+        G.add_edge(edge_from, edge_to)
+
+    return G
+
+#------------------------------------------------------------------------------#
+# Action Functions
+#------------------------------------------------------------------------------#
+def sample_node(G):
+    nodes = G.nodes()
+    return nodes[rand.randint(0, len(nodes))]
+
+def add_node(G):
+    if G.node_holes:
+        G.add_node(G.node_holes.pop())
+    else:
+        G.add_node(G.number_of_nodes())
+
+def add_node_random_edge(G):
+
+    if G.node_holes:
+        node_label = G.node_holes.pop()
+    else:
+        node_label = G.number_of_nodes()
+
+    random_existing_node = sample_node(G)
+    G.add_node(node_label)
+    G.add_edge(node_label, random_existing_node)
+
+def delete_node_random(G):
+    if G.number_of_nodes() > 1:
+        #node_index = rand.randint(G.number_of_nodes())
+        #node_label = G.nodes()[node_index]
+        node_label = sample_node(G)
+        G.node_holes.append(node_label)
+        G.remove_node(node_label)
+
+
+def delete_node_in_degree(G):
+    if G.number_of_nodes() > 1:
+        in_degree = np.array(G.in_degree().values())
+        if in_degree.max() > 0:
+            pmf = in_degree.astype(float) / in_degree.sum()
+
+            node_index = sample_pmf(pmf)
+            node_label = G.nodes()[node_index]
+
+            G.node_holes.append(node_label)
+            G.remove_node(node_label)
+
+def delete_node_in_degree_inverse(G):
+    if G.number_of_nodes() > 1:
+        inv_in_degree = 1 / (np.array(G.in_degree().values()) + 1)
+        pmf = inv_in_degree.astype(float) / inv_in_degree.sum()
+
+        #node_index = sample_pmf(pmf)
+        #node_label = G.nodes()[node_index]
+        node_label = sample_node(G)
+
+        G.node_holes.append(node_label)
+        G.remove_node(node_label)
+
+def add_edge_random(G):
+    edge_from_index = rand.randint(0,G.number_of_nodes())
+    edge_from_label = G.nodes()[edge_from_index]
+
+    edge_to_index   = rand.randint(0,G.number_of_nodes())
+    edge_to_label   = G.nodes()[edge_to_index]
+    G.add_edge(edge_from_label, edge_to_label)
+
+def add_edge_in_degree(G):
+    in_degree = np.array(G.in_degree().values())
+
+    if in_degree.max() > 0 :
+        edge_from_index = rand.randint(0,G.number_of_nodes())
+        edge_from_label = G.nodes()[edge_from_index]
+
+        pmf = in_degree.astype(float) / in_degree.sum()
+
+        edge_to_index = sample_pmf(pmf)
+        edge_to_label = G.nodes()[edge_to_index]
+
+        G.add_edge(edge_to_label, edge_from_label)
+    else:
+        add_random_edge(G)
+
+def delete_edge_random(G):
+    if G.number_of_edges() > 0:
+        edge_index = rand.randint(0, G.number_of_edges())
+        G.remove_edge(*G.edges()[edge_index])
+
+#------------------------------------------------------------------------------#
+# Feature Functions
+#------------------------------------------------------------------------------#
+def num_nodes(G):
+    return float(G.number_of_nodes()) / 1000.0
+
+def num_edges(G):
+    return float(G.size()) / 1000.0
+
+def diameter(G):
+    return nx.diameter(G)
+
+def average_clustering(G):
+    return nx.average_clustering(G)
+
+def average_in_degree(G):
+    return np.array(G.in_degree().values()).mean()
+
+def powerlaw_mle(G):
+    return fit_powerlaw_regress(G)[0]
+
+#def average_out_degree(G):
+#    return np.array(G.out_degree().values()).mean()
+
+
+glearn = gg.gglearner(initial_graph(3,2),
+                      reward_function_gen(target_indegree_exponent, target_num_nodes),
+                      [add_node_random_edge, add_edge_random, add_edge_in_degree],
+                      ["add node",
+                       "add random edge", "add edge by in-degree"],
+                      basis_functions,
+                      [num_nodes, num_edges, average_in_degree],#, powerlaw_mle],
+                      termination_function,
+                      10000)
+
+
+#glearn.episode(0.9, 0.9, 0.9, True)
+glearn.run_episode(0.9,0.9,0.9)
